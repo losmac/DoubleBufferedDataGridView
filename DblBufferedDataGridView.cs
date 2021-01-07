be1c118910560035ebdbf72a44ea11c0ba25c6f0
Maciej Łoś <38184922+losmac@users.noreply.github.com>
2021-01-07 09:43:03 +0100

    Add files via upload

---
+++ DblBufferedDataGridView.cs
@@ -0,0 +1,25 @@
+﻿//using System.ComponentModel;
+//using System.ComponentModel.Design;
+using System.Windows.Forms;
+//using ComponentFactory.Krypton.Toolkit;
+
+namespace TestDoubleBufferedDataGridView
+{
+    //[Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    public partial class DblBufferedDataGridView : DataGridView
+    {
+        public DblBufferedDataGridView()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            //this.CellPainting += CustomCellPainting;
+        }
+
+        private void CustomCellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            //DataGridViewCell dgc = this.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            //this.Rows[e.RowIndex].DefaultCellStyle.BackColor = e.RowIndex % 2 == 0 ? Color.White : Color.Ivory;
+        }
+        
+    }
+}
